d7c2e35d44c27a95ac423a0a05271c33959a6ec3
Nurrizky <nurrizkyimani@mail.ugm.ac.id>
2024-06-16 13:09:13 +0000

    add: search in rotated sorted array

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,4 +3,39 @@ public class Main {
     System.out.println("Hello world!");
   }
 
+
+}
+
+
+// 33. Search in Rotated Sorted Array
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+class Solution {
+  public int search(int[] nums, int target) {
+      int left = 0; 
+      int right = nums.length -1; 
+
+      while(left <= right){
+          int mid = left + (right - left) /2; 
+
+          if(nums[mid] == target){
+              return mid; 
+          } else if (nums[mid] >= nums[left]){
+              if(target >= nums[left]  && target < nums[mid]){
+                  right = mid -1; 
+              } else {
+                  left = mid +1; 
+              }
+          } else {
+              if(target <= nums[right] && target > nums[mid]){
+                  left = mid + 1; 
+              } else {
+                  right = mid - 1; 
+              }
+          }
+
+      }
+
+      return -1; 
+  }
+
 }
