42f0e237795cfaedcc1d73b4e10a83b7c55d343b
Nurrizky <nurrizkyimani@mail.ugm.ac.id>
2024-06-19 22:07:02 +0000

    add: 1091. Shortest Path in Binary Matrix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -124,4 +124,66 @@ class Solution {
 
         return res; 
     }
+
+    // https://leetcode.com/problems/shortest-path-in-binary-matrix/
+    // Given an n x n binary matrix grid, return the length of the shortest clear path in the matrix. If there is no clear path, return -1.
+
+    // A clear path in a binary matrix is a path from the top-left cell (i.e., (0, 0)) to the bottom-right cell (i.e., (n - 1, n - 1)) such that:
+
+    // All the visited cells of the path are 0.
+    // All the adjacent cells of the path are 8-directionally connected (i.e., they are different and they share an edge or a corner).
+    // The length of a clear path is the number of visited cells of this path.
+
+    private static final int[][] dirs = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        if (grid[0][0] != 0 || grid[grid.length - 1][grid[0].length - 1] != 0) {
+            return -1;
+        }
+
+           Queue<int[]> q = new ArrayDeque<int[]>(); 
+           grid[0][0] = 1; 
+           q.add(new int[]{0,0});
+
+            while(!q.isEmpty()){
+                int[] cell = q.remove();
+                int row = cell[0];
+                int col = cell[1];
+
+
+                int dist = grid[row][col];
+                if(row == grid.length -1 && col == grid[0].length -1) return dist; 
+
+
+                for(int[] neigh: getNeighboList(grid, row, col)){
+                    int neighRow = neigh[0];
+                    int neighCol = neigh[1];
+
+                    q.add(new int[]{neighRow, neighCol});
+                    grid[neighRow][neighCol] = dist + 1; 
+                }
+           }
+
+           return -1; 
+
+
+    }
+
+    public List<int[]> getNeighboList(int[][] grid, int row, int col){
+
+        List<int[]>  neigh = new ArrayList<>();
+
+        for (int[] dir : dirs) {
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+
+            if(newRow < 0 || newCol < 0 || newRow >= grid.length || newCol >= grid[0].length || grid[newRow][newCol] != 0){
+                continue; 
+            }
+
+            neigh.add(new int[]{newRow, newCol});
+        }
+
+        return neigh; 
+    }
 }
